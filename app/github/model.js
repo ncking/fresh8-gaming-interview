8f446359d0eecd1c348627d1b08bc92ab6649e55
Nigel <Nigel.c.king@gmail.com>
2017-01-17 10:02:24 +0000

    model.getStars to return INT

--- app/github/model.js
+++ app/github/model.js
@@ -14,7 +14,7 @@ module.exports = function (data) {
          * 
          */
         getStars: function () {
-            return Number(this.get('stargazers_count'));
+            return Math.floor(this.get('stargazers_count'));
         }
     }
 }
